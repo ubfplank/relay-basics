215ca082ea4b9a3e0c8df752b83b398bacbeba5c
Thiago Mota Santos <lucimara1422@gmail.com>
2023-04-12 13:05:39 -0300

    relay-basics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { RelayEnvironmentProvider } from "react-relay";
+import { initRelayEnvironment } from "../RelayEnvironment";
+import { RecordSource } from "relay-runtime";
+import { useMemo, useEffect } from "react";
+import { RelayPageProps } from "../relay-types";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<RelayPageProps>) {
+  const environment = useMemo(initRelayEnvironment, []);
+
+  useEffect(() => {
+    const store = environment.getStore();
+
+    store.publish(new RecordSource(pageProps.initialRecords));
+
+    store.notify();
+  }, [environment, pageProps.initialRecords]);
+
+  return (
+    <RelayEnvironmentProvider environment={environment}>
+      <Component {...pageProps} />
+    </RelayEnvironmentProvider>
+  );
 }
